30bf444d68821a60fcbcc54111274f2e6168f815
Tommy Huang <htommyf12@gmail.com>
2020-07-21 15:07:09 +0800

    added a lot of stuff, still broken

--- game.py
+++ game.py
@@ -3,63 +3,136 @@ import pandas as pd
 import os
 import time
 
-os.system("color f0")
+# os.system("color f0")
 path = os.path.abspath("questions.csv")
 df = pd.read_csv(path)
 
 
-def pick(i):
-    print(df['題目'][i])
-    print("A:{} B:{} C:{} D:{}\n".format(
-        df["A選項"][i], df["B選項"][i], df["C選項"][i], df["D選項"][i]))
+class Question:
+    """
+    The Question class takes 1 arguement and contains 6 attributes\n
+
+    Arguement:\n
+        \tindex: An int specifying the index of the question from the data\n
+    Attributes:\n
+        \tquestion: The question itself
+        \tA: Option A
+        \tB: Option B
+        \tC: Option C
+        \tD: Option D
+        \tanswer: The answer to the question
+    """
+
+    def __init__(self, index: int):
+        self.question: str = df["題目"][index]
+        self.A: str = df["A選項"][index]
+        self.B: str = df["B選項"][index]
+        self.C: str = df["C選項"][index]
+        self.D: str = df["D選項"][index]
+        self.answer: str = df["答案"][index]
+
+    def printQuestion(self):
+        print(self.question)
+        print(f"A:{self.A} B:{self.B} C:{self.C} D:{self.D}")
 
 
-def check(i, e, f, g):
+class ActivationCard:
+    """
+    A base class for all activation cards
+    """
+
+    def __init__(self):
+        self.isUsed = False
+
+    def useCard(self, question: Question):
+        """
+        If a card has not been activated call each cards _activate() method.\n
+        Else, inform the player the card has been activated.
+        """
+        if self.isUsed is False:
+            self.isUsed = True
+            self._activate(question)
+        else:
+            print("\n此求救卡已使用過\n")
+
+
+class ChangeQuestionCard(ActivationCard):
+    """
+    Skips the current question without answering or penalty
+    """
+
+    def __init__(self):
+        super().__init__()
+
+    def _activate(self):
+        print("test")
+
+
+class SecondChanceCard(ActivationCard):
+    """
+    Offers a second answering chance if the answer is incorrect\n
+    *Needs to be activated before answering so could be a waste
+    """
+
+    def __init__(self):
+        super().__init__()
+        self.activated = False
+
+    def _activate(self):
+        pass
+
+
+class RemoveOptionCard(ActivationCard):
+    def __init__(self):
+        super().__init__()
+
+    def _activate(self, question: Question):
+        # show one wrong option by convert correct answer to ASCII code and add a random num then convert back to chr
+        wrongOption = chr(
+            (ord(question.answer) + randint(0, 2)) % 4 + 65)
+
+        print(f"\n{wrongOption}選項是錯的\n")
+
+
+def check(question: Question, e, f: RemoveOptionCard, g):
     activate = False
     while True:
-        # print(df["答案"][x])
-        a = input("請選擇答案:").upper()
+        answer = input("請選擇答案:").upper()
         b = False
-        options = ["A", "B", "C", "D"]
 
-        if len(a) == 1 and a[0] in "ABCDEFG":
+        if len(answer) == 1 and answer[0] in "ABCDEFG":
             # change question
-            if a[0] == "E" and e == False:
+            if answer[0] == "E" and e == False:
                 print("------------\n   換一題\n------------\n")
                 e = True
                 e, f, g, b = generate(e, f, g)
                 break
-
-            elif a[0] == "E" and e == True:
+            elif answer[0] == "E" and e == True:
                 print("\n此求救卡已使用過\n")
 
             # delete one option
-            if a[0] == "F" and f == False:
-                q = (options.index(df["答案"][i]) + randint(1, 3)) % 4
-                print(f"\n{options[q]}選項是錯的\n")
-                f = True
-            elif a[0] == "F" and f == True:
-                print("\n此求救卡已使用過\n")
+            if answer[0] == "F":
+                f.useCard(question)
 
             # second chance
-            if a[0] == "G" and g == False:
+            if answer[0] == "G" and g == False:
                 print("\n啟用第二次機會\n")
                 g = True
                 activate = True
-            elif a[0] == "G" and g == True:
+            elif answer[0] == "G" and g == True:
                 print("\n此求救卡已使用過\n")
 
             # check answer
-            if a[0] == df["答案"][i]:
+            if answer[0] == question.answer:
                 print("\n答案正確\n")
                 b = True
                 break
-            elif a[0] != df["答案"][i] and activate == True:
+            elif answer[0] != question.answer and activate == True:
                 print("\n答案錯誤，還有一次機會\n")
                 activate = False
             else:
                 print("\n答案錯誤\n")
-                print("正確答案應為:{}\n".format(df["答案"][i]))
+                print(f"正確答案應為:{question.answer}\n")
                 b = False
                 break
 
@@ -69,48 +142,55 @@ def check(i, e, f, g):
     return b, e, f, g
 
 
-def generate(e, f, g):
+def generate(e: ChangeQuestionCard, f: RemoveOptionCard, g: SecondChanceCard):
     i = randint(0, len(df))
-    pick(i)
-    b, e, f, g = check(i, e, f, g)
+    question = Question(i)
+    question.printQuestion()
+    b, e, f, g = check(question, e, f, g)
     if b == False:
         return e, f, g, False
     return e, f, g, True
 
 
-while True:
-    e_used = False
-    f_used = False
-    g_used = False
+def start():
+    changeQuestionCard = ChangeQuestionCard()
+    removeOptionCard = RemoveOptionCard()
+    secondChanceCard = SecondChanceCard()
 
-    print("遊戲開始")
-    print("輸入A、B、C、D作答，E、F、G啟用求救卡\nE:更換題目  F:刪去選項  G:第二條命\n你準備好了嗎?")
-    time.sleep(1)
-    for i in range(4):
-        print(3 - i)
-        time.sleep(2)
+    # game starting sequence
+    # TODO: add this back
+    # print("遊戲開始")
+    # print("輸入A、B、C、D作答，E、F、G啟用求救卡\nE:更換題目  F:刪去選項  G:第二條命\n你準備好了嗎?")
+    # time.sleep(1)
+    # for i in range(4):
+    #     print(3 - i)
+    #     time.sleep(2)
 
     for m in range(10):
         print("第{}題:\n".format(m + 1))
 
-        e_used, f_used, g_used, b = generate(e_used, f_used, g_used)
+        e_used, f_used, g_used, b = generate(
+            changeQuestionCard, removeOptionCard, secondChanceCard)
 
         if m == 9:
             print("恭喜挑戰成功\n")
-            break
+            return None
         elif b == False:
-            break
+            print("\nGAME OVER\n")
+            return None
         else:
             print("------------\n   下一題\n------------\n")
 
-    while True:
-        print("\nGAME OVER\n")
-        i = input("\n是否要再來一局?(y/n) ").lower()
-        if i == "n":
-            os.system("pause")
-            exit(0)
-        elif i == "y":
-            os.system("cls")
-            break
-        else:
-            print("資料錯誤，請重新輸入\n")
+
+while True:
+    start()
+
+    i = input("\n是否要再來一局?(y/n)").lower()
+    if i == "n":
+        os.system("pause")
+        exit(0)
+    elif i == "y":
+        os.system("cls")
+        break
+    else:
+        print("資料錯誤，請重新輸入\n")
